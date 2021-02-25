66417e33b9e6979ea66d7e5270bc7c3413bb31d8
amir <amir321321+>
2021-02-25 10:14:46 +0100

    ready for deployment

---
+++ client/build/precache-manifest.072a61dca6622e4bd99ca6e7655aeec5.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9db54a3d74637ca176177f1a0a0704e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c312ae7f6813a5ac690",
+    "url": "/static/css/main.b590b6e4.chunk.css"
+  },
+  {
+    "revision": "f06ac6a19db4b9b58b3f",
+    "url": "/static/js/2.0856f62a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.0856f62a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c312ae7f6813a5ac690",
+    "url": "/static/js/main.bd377db9.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "5965b7d60e16fb862ab9bff7742340ae",
+    "url": "/static/media/Elec Eco 02-2021 V1.5965b7d6.pdf"
+  },
+  {
+    "revision": "165d90eb51ccfcebb6318353dc2e47bb",
+    "url": "/static/media/Elec EcoGreen 02-2021 V1.165d90eb.pdf"
+  },
+  {
+    "revision": "92077daac5f56cee364990db0a5ce89a",
+    "url": "/static/media/Gaz ECO 01022021 V1.92077daa.pdf"
+  },
+  {
+    "revision": "3d8f8fd78fd85a4d6e3dd32e27f0a820",
+    "url": "/static/media/Gaz FIXE 01022021 V1.3d8f8fd7.pdf"
+  },
+  {
+    "revision": "e7c89dd427722db6211579d6c7321485",
+    "url": "/static/media/cvg.e7c89dd4.pdf"
+  },
+  {
+    "revision": "0314396792e24084bff216b48b1332e9",
+    "url": "/static/media/fiche-descriptive-electricite-eco-01012021.03143967.pdf"
+  },
+  {
+    "revision": "1ab5e529abcc16c604ae15f4d342ca52",
+    "url": "/static/media/fiche-descriptive-electricite-eco-green-vattenfall01012021.1ab5e529.pdf"
+  },
+  {
+    "revision": "932b8dc995849bca08a3073d571eaba7",
+    "url": "/static/media/fiche-descriptive-gaz-eco-01012021.932b8dc9.pdf"
+  },
+  {
+    "revision": "80cf8d4612fb462ee7c1cde6ef59a26e",
+    "url": "/static/media/fiche-descriptive-gaz-fixe-36-mois-01012021.80cf8d46.pdf"
+  },
+  {
+    "revision": "1aa058ee581d97ff14c907be3ec04e59",
+    "url": "/static/media/icon1.1aa058ee.png"
+  },
+  {
+    "revision": "718026dafa289c9eb2fffd3de0813f56",
+    "url": "/static/media/icon2.718026da.png"
+  },
+  {
+    "revision": "63a0c87458c226cb631e23fa4fcab562",
+    "url": "/static/media/icon4.63a0c874.png"
+  },
+  {
+    "revision": "9ad9f503de3f014d19d486079ac6eac1",
+    "url": "/static/media/icon5.9ad9f503.png"
+  },
+  {
+    "revision": "884d06bc60e008e7b2e0466efbb4842a",
+    "url": "/static/media/landing.884d06bc.jpg"
+  },
+  {
+    "revision": "64b6ac0a544ebacaca7ab0b6a0949786",
+    "url": "/static/media/log1.64b6ac0a.png"
+  },
+  {
+    "revision": "745705b7ed8ab85bd4b2fcdeddbd6268",
+    "url": "/static/media/road.745705b7.jpeg"
+  }
+]);
---
+++ models/Form.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const formSchema = Schema({
+    fullName: {
+        type: String,
+      
+    },
+    type: {
+        type: String,
+     
+    },
+    label: {
+        type: String,
+
+    },
+    monthlyPayment: {
+        type: String,
+     
+    },
+    adress: {
+        type: String,
+    },
+    negothiablePayment: {
+        type: String,
+    },
+    poneNumber: {
+        type: String,
+        
+    },
+    pdl: {
+        type: String,
+    },
+    city: {
+        type: String,
+    },
+    pce: {
+        type: String,
+    },
+    postalCode: {
+        type: String,
+    },
+    email: {
+        type: String,
+        required: true,
+
+    },
+    consomEstimation: {
+        type: String,
+    }
+});
+
+module.exports = mongoose.model('form', formSchema)
