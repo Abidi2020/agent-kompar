9b2f38027ac1b0995dfe1b995d7340afcaceae88
amir <amir321321+>
2021-02-25 10:27:29 +0100

    ready

--- server.js
+++ server.js
@@ -15,6 +15,16 @@ app.use('/api/auth', require('./routes/api/authRoute'))
 app.use('/api/form', require('./routes/api/formRoute'))
 app.use('/api/test', require('./routes/api/testRoute'))
 
+// Serve  static assets in production
+if(process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'))
+
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
 // Listen to the servere
 const PORT = process.env.PORT || 5000;
 
